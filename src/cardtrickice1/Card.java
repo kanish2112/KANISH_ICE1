35a4e7736bc5999824ce2dc2d6d481793188890b
kanish2112 <105872753+kanish2112@users.noreply.github.com>
2022-06-01 11:46:05 -0400

    Update Card.java

--- src/cardtrickice1/Card.java
+++ src/cardtrickice1/Card.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * MADE BY-KANISH KRUPALKUMAR BHAVSAR STUDENT ID-991666094
  */
 package cardtrickice1;
 
